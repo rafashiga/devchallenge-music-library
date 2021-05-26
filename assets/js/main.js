5097cf73b6b8a221ece29a74767569ef9777ae99
Rafael <rafa.shiga@gmail.com>
2021-05-26 10:06:45 -0300

    fix: adjust show and hide sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 const menuBtn = document.querySelector('.menu');
 
+const body = document.querySelector('body');
+
+const currentWidth =
+	window.innerWidth ||
+	document.documentElement.clientWidth ||
+	document.body.clientWidth;
+
+function showSidebar() {
+	body.classList.add('show-sidebar');
+	body.classList.remove('hide-sidebar');
+}
+
 menuBtn.addEventListener('click', () => {
-	const body = document.querySelector('body');
 	if (body.className === 'hide-sidebar') {
-		body.classList.add('show-sidebar');
-		body.classList.remove('hide-sidebar');
+		showSidebar();
+	} else if (body.className.length === 0 && currentWidth < 768) {
+		showSidebar();
 	} else {
 		body.classList.remove('show-sidebar');
 		body.classList.add('hide-sidebar');
